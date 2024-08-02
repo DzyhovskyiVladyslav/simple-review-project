4d8472942b6cd1e3562d00f722b8927772b62a22
DzyhovskyiVladyslav <dzyhovskyi.vladyslav@gmail.com>
2024-08-02 11:04:07 +0300

    commit001

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'simple-review-project';
+export class AppComponent implements OnInit{
+  myForm: FormGroup = new FormGroup({});
+  name = "";
+  email = "";
+  product = "";
+  rate = "";
+  review = "";
+
+  constructor(private formBuilder: FormBuilder){}
+  ngOnInit(): void {
+    this.myForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      product: ['', [Validators.required, Validators.pattern('^[0-9]+$'), Validators.minLength(6), Validators.maxLength(6)]],
+      rate: ['', [Validators.required, Validators.pattern('^[0-9]+$'), Validators.min(0), Validators.max(5)]],
+      review: ['', [Validators.required]],
+    })
+  }
+
+  submitForm() {
+    console.log( "name: " + this.name + 
+      "\nemail: " + this.email +
+      "\nproduct: " + this.product +
+      "\nrate: " + this.rate +
+      "\nreview: " + this.review);
+  }
 }
